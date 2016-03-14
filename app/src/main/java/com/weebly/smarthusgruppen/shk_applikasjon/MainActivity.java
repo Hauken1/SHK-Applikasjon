c81f9d4bf41757b4e9f00e831d4c2baf302e99c7
wrajajar <nosfera.gnewm@hotmail.com>
2016-03-14 13:35:44 +0100

    connectToServer
    Første utkast av connectToServer frå client

--- app/src/main/java/com/weebly/smarthusgruppen/shk_applikasjon/MainActivity.java
+++ app/src/main/java/com/weebly/smarthusgruppen/shk_applikasjon/MainActivity.java
@@ -1,15 +1,116 @@
 package com.weebly.smarthusgruppen.shk_applikasjon;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+
 public class MainActivity extends AppCompatActivity {
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        connectToServer();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+    private void connectToServer() {
+        int portNumber = 4004;
+        String hostName= "127.0.0.1";
+        String textToSend = "Heisann Henrik";
+        Socket myClient;
+        BufferedWriter output;
+        BufferedReader input;
 
 
+        try {
+            myClient = new Socket(hostName, portNumber);
+            output = new BufferedWriter(new OutputStreamWriter(
+                    myClient.getOutputStream()));
+            input = new BufferedReader(new InputStreamReader(
+                    myClient.getInputStream()));
+
+            output.write(textToSend);
+            output.newLine();
+            output.flush();
+
+            output.close();
+            input.close();
+            myClient.close();
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+        // Creating socket and checking for exceptions
+        // Creating output stream to send information
+        // Creating input stream to receive information
+        // closing sockets and socket after use
+
+        /*String hostName = args[0];
+        int portNumber = Integer.parseInt(args[1]); */
+
+
+    }
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.weebly.smarthusgruppen.shk_applikasjon/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.weebly.smarthusgruppen.shk_applikasjon/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
     }
 }
