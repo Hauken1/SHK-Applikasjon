bd2369856b8b44498d83cb8b1f5038a2555ee45d
Hauken1 <henrik.hauk.92@gmail.com>
2016-05-06 14:01:55 +0200

    ekstra

--- app/src/main/java/com/weebly/smarthusgruppen/shk_applikasjon/LoginClient.java
+++ app/src/main/java/com/weebly/smarthusgruppen/shk_applikasjon/LoginClient.java
@@ -52,7 +52,7 @@ public class LoginClient extends AppCompatActivity {
     TextView forgotPw;
 
     int serverPort = 12345;
-    String hostName= "192.168.38.110";
+    String hostName= "128.39.80.218";
     // 128.39.81.160 10.0.2.2
     static BufferedWriter output;
     static BufferedReader input;
@@ -721,6 +721,8 @@ public class LoginClient extends AppCompatActivity {
                                         }
 
                                     }
+                                    pDialog.dismiss();
+                                    settingsDialog.dismiss();
                                 }
                             }));
                             thread.start();
